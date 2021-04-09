bc73b68c95d95a470a9df9953d88f99662bac440
hulya <hulyakarakayaa06@gmail.com>
2021-04-09 12:42:53 -0700

    create the header avatar with custom styles

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,10 +1,10 @@
-const Avatar = ({ url }) => {
+const Avatar = ({ url, className }) => {
     return (
         <img
             loading="lazy"
             src={url}
             alt="profile"
-            className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110"
+            className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
         />
     );
 };
