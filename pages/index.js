dd2301200c03669d290efe5007fce66a8fb8cb62
hulya <hulyakarakayaa06@gmail.com>
2021-04-08 19:14:01 -0700

    create the body with form

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,19 @@
 import Head from 'next/head';
 import Avatar from '../components/Avatar';
+import { Footer } from '../components/Footer';
 import { ViewGridIcon, MicrophoneIcon } from '@heroicons/react/solid';
+import { SearchIcon } from '@heroicons/react/outline';
+import Image from 'next/image';
 
 export default function Home() {
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center h-screen">
             <Head>
                 <title>Google</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+
+            {/* Header */}
             <header className="flex w-full p-5 justify-between text-sm text-gray-700">
                 {/* left  */}
                 <div className="flex space-x-4 items-center">
@@ -22,14 +27,29 @@ export default function Home() {
                     {/* Icons */}
                     <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
                     {/* Avatar */}
-                    <Avatar
-                        url={
-                            'https://pbs.twimg.com/profile_images/1373459766790397952/wr_a_1nv_400x400.jpg'
-                        }
-                    />
+                    <Avatar url="https://pbs.twimg.com/profile_images/1373459766790397952/wr_a_1nv_400x400.jpg" />
                 </div>
             </header>
-            <body></body>
+
+            {/* Body */}
+            <form className="flex flex-col items-center mt-40 flex-grow w-4/5">
+                <Image
+                    src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+                    height={100}
+                    width={300}
+                />
+                <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg mx-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+                    <SearchIcon className="h-5 mr-3 text-gray-500" />
+                    <input className="flex-grow focus:outline-none" type="text" />
+                    <MicrophoneIcon className="h-5" />
+                </div>
+                <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+                    <button className="btn">Google Search</button>
+                    <button className="btn">I'm Feeling Lucky</button>
+                </div>
+            </form>
+            {/* Footer */}
+            <Footer />
         </div>
     );
 }
