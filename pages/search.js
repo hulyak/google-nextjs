a4848decafcde0bba78825bddca1ec2a8ac218df
hulya <hulyakarakayaa06@gmail.com>
2021-04-09 16:05:21 -0700

    update the search url

--- pages/search.js
+++ pages/search.js
@@ -31,7 +31,7 @@ export async function getServerSideProps(context) {
     const data = useDummyData
         ? Response
         : await fetch(
-              `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q={context.query.term}&start=${startIndex}`
+              `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
           ).then((response) => response.json());
     // after the server has rendered, pass the results to the client
     return {
